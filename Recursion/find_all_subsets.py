4a45c2b26c30f29753ed69e626d62fdf27915fd4
at669266 <arpansunil.tolat@broadcom.com>
2019-12-02 19:57:01 -0800

    sunset

---
+++ Recursion/find_all_subsets.py
@@ -0,0 +1,48 @@
+# 78. Subsets
+# Medium
+
+# Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+# Note: The solution set must not contain duplicate subsets.
+
+# Example:
+
+# Input: nums = [1,2,3]
+# Output:
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def helper(S, i, slate):
+            # Base Case
+            if len(S) == i:
+                result.append(slate[:])
+                return
+            
+            # Inner Nodes
+            # Inclusion
+            slate.append(S[i])
+            helper(S, i+1, slate)
+            slate.pop()
+    
+            # Exclusion
+            helper(S, i+1, slate)
+        
+        result = []
+        helper(nums, 0, [])
+        return result
+    
+    
