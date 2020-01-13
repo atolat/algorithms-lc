ed108560fb8d98c529b8ab0ddc17fd3d0d0ad133
atolat <arpantolat30@gmail.com>
2020-01-13 09:27:46 -0800

    lc 207 - course schedule

---
+++ Graphs/course-schedule-I.py
@@ -0,0 +1,64 @@
+# 207. Course Schedule
+# Medium
+
+# There are a total of n courses you have to take, labeled from 0 to n-1.
+
+# Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+# Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+# Example 1:
+
+# Input: 2, [[1,0]] 
+# Output: true
+# Explanation: There are a total of 2 courses to take. 
+#              To take course 1 you should have finished course 0. So it is possible.
+# Example 2:
+
+# Input: 2, [[1,0],[0,1]]
+# Output: false
+# Explanation: There are a total of 2 courses to take. 
+#              To take course 1 you should have finished course 0, and to take course 0 you should
+#              also have finished course 1. So it is impossible.
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        # Initialize visited, arrival, departure, parent arrays, adjList, timestamp
+        n = numCourses
+        adjList = [[] for _ in range(n)]
+        visited = [-1] * n
+        arrival = [-1] * n
+        departure = [-1] * n
+        timestamp = [0]
+        
+        # Construct the graph
+        for (src,dst) in prerequisites:
+            adjList[dst].append(src)
+            
+        # DFS
+        def dfs(source):
+            arrival[source] = timestamp[0]
+            timestamp[0] += 1
+            visited[source] = 1
+            for neighbor in adjList[source]:
+                if visited[neighbor] == -1:
+                    if dfs(neighbor):
+                        return True
+                else:
+                    if departure[neighbor] == -1: # departure is absent
+                        return True
+            departure[source] = timestamp[0]
+            timestamp[0] += 1
+            return False
+        
+        # Outer Loop
+        for v in range(n):
+            if visited[v] == -1:
+                if dfs(v):
+                    return False
+        return True
