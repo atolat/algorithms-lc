0e6c23bf951db1232b0cd6071b96afbfd37a2281
atolat <arpantolat30@gmail.com>
2020-01-15 15:17:47 -0800

    lc 1099

---
+++ Strings-Arrays/two-sum-less-K.py
@@ -0,0 +1,44 @@
+# 1099. Two Sum Less Than K
+# Easy
+
+# Given an array A of integers and integer K, return the maximum S such that there exists i < j with A[i] + A[j] = S and S < K. If no i, j exist satisfying this equation, return -1.
+# Example 1:
+
+# Input: A = [34,23,1,24,75,33,54,8], K = 60
+# Output: 58
+# Explanation: 
+# We can use 34 and 24 to sum 58 which is less than 60.
+# Example 2:
+
+# Input: A = [10,20,30], K = 15
+# Output: -1
+# Explanation: 
+# In this case it's not possible to get a pair sum less that 15.
+
+class Solution(object):
+    def twoSumLessThanK(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        # Sort input array
+        A.sort()
+        result = -1
+        # Initialize two pointers
+        start = 0
+        end = len(A)-1
+        
+        while start < end:
+            s = A[start] + A[end]
+            if s > K:
+                end -= 1
+            elif s < K:
+                start += 1
+                if s > result:
+                    result = s
+            else:
+                end -= 1
+
+                    
+        return result
