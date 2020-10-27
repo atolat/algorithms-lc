155c098ad790f158a608b8e07329bedf99612c22
atolat <arpantolat30@gmail.com>
2020-10-26 18:25:16 -0700

    Update

--- DP/coin-change.py
+++ DP/coin-change.py
@@ -1,19 +1,23 @@
-# 322. Coin Change
-# Medium
-
-# You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
-
-# Example 1:
-
-# Input: coins = [1, 2, 5], amount = 11
-# Output: 3 
-# Explanation: 11 = 5 + 5 + 1
-# Example 2:
-
-# Input: coins = [2], amount = 3
-# Output: -1
-# Note:
-# You may assume that you have an infinite number of each kind of coin.
+# Time Complexity - O(n) where n = amount
+# Space Complexity - O(n)
+# Did this run on Leetcode - Yes
+# Did you face any problems - No
+# Approach & Intuition
+# Try to decrease and conquer the problem:
+# - Define an objective function f(i) = minimum amount of coins to make amount i
+# - For any arbitrary amount i, we can compute f(i) using the min of the previous solutions + 1
+# - For instance - given coin denominations 1,2,5 and amount 11
+# - We can get 11 using min(f(11-1), f(11-2), f(11-5)) + 1
+# - This leads to the following recurrence equation: f(i) = min(f(i-ck))+1 ∀ c ∈ coins
+# - If we construction the recurrence tree for this on the given example
+# - f(11) depends on f(10), f(9), f(6)
+#     - f(10) depends on f(9), f(8), f(5)
+#     - f(9) depends on f(8), f(7), f(4)
+#     - f(6) depends on f(5), f(4), f(1)
+#         - f(9) depends on f(8), f(7), f(4)
+#         ...
+# - There are multiple repeating subproblems here which makes it a good candidate for DP
+# - We can build a 1D dp table to hold solutions to all subproblems and populate it following the recurrance.
 
 class Solution(object):
     def coinChange(self, coins, amount):
@@ -23,19 +27,20 @@ class Solution(object):
         :rtype: int
         """
         # Initialize 1D array of size amount
-        # Every value in the dp table represents:
-        # table[i] = min number of coins to make amount i
         table = [float('+inf')]*(amount+1)
-        
+
         # Base Cases
         table[0] = 0
-        
+
         # Tabulation
         for i in range(1, amount+1):
             minval = float('+inf')
+            # For every coin denomination available
+            # If it is >= 0, lookup it's values, find the min
             for c in coins:
                 if i - c >= 0:
                     minval = min(table[i-c], minval)
+            # Add the min + 1 (including the last coin) to fill the table
             table[i] = minval + 1
-            
+
         return table[-1] if table[-1] != float('+inf') else -1
--- DP/house-robber.py
+++ DP/house-robber.py
@@ -1,22 +1,17 @@
-# 198. House Robber
-# Easy
-
-# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
-
-# Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
-
-# Example 1:
-
-# Input: nums = [1,2,3,1]
-# Output: 4
-# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-#              Total amount you can rob = 1 + 3 = 4.
-# Example 2:
-
-# Input: nums = [2,7,9,3,1]
-# Output: 12
-# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-#              Total amount you can rob = 2 + 9 + 1 = 12.
+# Time Complexity - O(n) where n = # of houses
+# Space Complexity - O(n)
+# Did this run on Leetcode - Yes
+# Did you face any problems - No
+# Approach & Intuition
+# Try to decrease and conquer the problem:
+# - Define an objective function f(i) = maximum amount of money you can rob from the first i houses
+# - At the ith house, the robber can make 1 of 2 choices - Rob the last house or not rob the last house
+# - This leads to the following recurrence equation: f(i) = max(f(i-1), f(i-2) + nums[i])
+# - The recurrence tree for this would look similar to the recurrance for the fibonacci series
+# where the solution to the current problem depends on the previous two subproblem solutions
+# - Recursion would lead to a combinatorial explosion O(2^n) of options (at every level inclde the current house and consult the solution for the i-2th subproblem or exclude the current house and consult the solution for i-1th subproblem)
+# - There are multiple repeating subproblems here which makes it a good candidate for DP
+# - We can build a 1D dp table to hold solutions to all subproblems and populate it following the recurrance.
 
 class Solution(object):
     def rob(self, nums):
@@ -31,7 +26,6 @@ class Solution(object):
 
         # Initialize Table
         table = [None]*(len(nums)+1)
-        n = len(nums)
 
         # Base Case
         # Every entry in table - max amount of money you can rob from n houses
@@ -40,6 +34,8 @@ class Solution(object):
         table[2] = max(nums[0], nums[1])  # Cannot rob two adjacent houses
 
         # Fill table
-        for i in range(1, n):
-            table[i + 1] = max(nums[i] + table[i - 1], table[i])
-        return table[n]
+        # If the. last house is robbed, max profit is f(i-2)+nums[i]
+        # If the last house is not robbed the max profit is f(i-1)
+        for i in range(3, len(nums)+1):
+            table[i] = max(table[i-1], table[i-2] + nums[i-1])
+        return table[-1]
