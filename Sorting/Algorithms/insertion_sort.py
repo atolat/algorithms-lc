4d77135fa79a5d091540a33e1b791f91be9d334a
atolat <arpantolat30@gmail.com>
2021-02-22 18:05:47 -0800

    Re-org

--- Sorting/Algorithms/insertion_sort.py
+++ Sorting/Algorithms/insertion_sort.py
@@ -1,22 +1,22 @@
-def insertion_sort(arr):
-    return helper(arr, len(arr)-1)
+def insertionSort(array):
+    # Write your code here.
+	helper(array, len(array))
+	print(array)
 
 def helper(arr,n):
-    # Base Case
-    if n <= 1:
-        return
-    # Recursive Case
-    else:
-        helper(arr,n-1)
-        n_th = arr[n]
-        j = n-1
-        while j >= 1 and arr[j] > n_th:
-            arr[j+1] = arr[j]
-            j = j-1
-        arr[j+1] = n_th
-    return arr
-
-print(insertion_sort([1,2,1,1,11,6,5,4,3]))
+	# Base Case
+	if n <= 1:
+		return
+	# Recursive Case
+	helper(arr,n-1)
+	n_th = arr[n-1]
+	j = n-2
+	while j >= 0 and arr[j] > n_th:
+		arr[j+1] = arr[j]
+		j = j-1
+	arr[j+1] = n_th
+ 
+insertionSort([8, 5, 2, 9, 5, 6, 3])
 
 # Decrease and Conquer approach
 # T(n) = O(n**2)
